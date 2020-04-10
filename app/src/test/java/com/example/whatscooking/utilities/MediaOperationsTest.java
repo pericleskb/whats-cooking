64d615faf7bdc26f23251dd39f8e10bce07d6cb2
pericles <pericleskb@gmail.com>
2020-04-10 16:53:10 +0300

    first test

---
+++ app/src/test/java/com/example/whatscooking/utilities/MediaOperationsTest.java
@@ -0,0 +1,15 @@
+package com.example.whatscooking.utilities;
+
+import org.junit.Test;
+import static com.google.common.truth.Truth.assertThat;
+
+public class MediaOperationsTest {
+
+    @Test
+    public void getUniqueImageName_generateTwoDifferentNames() throws InterruptedException {
+        String name1 = MediaOperations.getUniqueImageName();
+        Thread.sleep(1000);
+        String name2 = MediaOperations.getUniqueImageName();
+        assertThat(name1).doesNotMatch(name2);
+    }
+}
