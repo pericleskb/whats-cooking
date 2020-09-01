bbbd0fea1cc3a23ba58dba9c00d35936010d19ed
pericles <pericleskb@gmail.com>
2020-09-01 15:04:34 +0300

    add null check for interceptChecker

--- app/src/main/java/com/example/whatscooking/main/recipepage/ScrollViewWithConditionalTouchIntercept.java
+++ app/src/main/java/com/example/whatscooking/main/recipepage/ScrollViewWithConditionalTouchIntercept.java
@@ -2,20 +2,12 @@ package com.example.whatscooking.main.recipepage;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ScrollView;
-
-import androidx.navigation.fragment.NavHostFragment;
 
 public class ScrollViewWithConditionalTouchIntercept extends ScrollView {
 
     TouchInterceptChecker interceptChecker = null;
-    final float scrollThreshold = 100;
-    float xStart = 0;
-    float yStart = 0;
-    float scrollViewStartPositionY = 0;
 
     public ScrollViewWithConditionalTouchIntercept(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -23,7 +15,10 @@ public class ScrollViewWithConditionalTouchIntercept extends ScrollView {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
-        return interceptChecker.shouldInterceptTouch((int) event.getX(), (int) event.getY());
+        if (interceptChecker != null) {
+            return interceptChecker.shouldInterceptTouch((int) event.getX(), (int) event.getY());
+        }
+        return false;
     }
 
     public void setInterceptChecker(TouchInterceptChecker interceptChecker) {
